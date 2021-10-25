cd1ad34022a4aa8832aa5edc657dc4d06c3fecbd
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-25 12:09:31 +0700

    testing MainViewModel.kt

---
+++ app/src/test/java/com/gunder/myunittesting/ui/model/MainViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.gunder.myunittesting.ui.model
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class MainViewModelTest {
+
+    @Test
+    fun getCircumFerence() {
+    }
+
+    @Test
+    fun getSurfaceArea() {
+    }
+
+    @Test
+    fun getVolume() {
+    }
+
+    @Test
+    fun save() {
+    }
+}
