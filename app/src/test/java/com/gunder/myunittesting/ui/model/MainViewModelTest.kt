7cee496316c42f4e56c29c413103e0aa22c92813
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-25 15:38:15 +0700

    menambah dependency utk testing dengan instrumentation

--- app/src/test/java/com/gunder/myunittesting/ui/model/MainViewModelTest.kt
+++ app/src/test/java/com/gunder/myunittesting/ui/model/MainViewModelTest.kt
@@ -1,10 +1,56 @@
 package com.gunder.myunittesting.ui.model
 
+import com.gunder.myunittesting.model.CuboidModel
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
+import org.mockito.Mockito.mock
 
 class MainViewModelTest {
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var cuboidModel: CuboidModel
+
+    private val dummyLength = 12.0
+    private val dummyWidth = 7.0
+    private val dummyHeight = 6.0
+
+    private val dummyVolume = 504.0
+    private val dummyCircumFerence = 100.0
+    private val dummySurfaceArea = 396.0
+
+    @Before
+    fun before() {
+        cuboidModel = mock(CuboidModel::class.java)
+        mainViewModel = MainViewModel(cuboidModel)
+    }
+
+    @Test
+    fun testVolume() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyHeight, dummyLength)
+        val volume = mainViewModel.getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+
+    @Test
+    fun testCircumFerence() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyLength, dummyHeight)
+        val volume = mainViewModel.getCircumFerence()
+        assertEquals(dummyCircumFerence, volume, 0.0001)
+    }
+
+    @Test
+    fun testSurfaceArea() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyWidth, dummyHeight, dummyLength)
+        val volume = mainViewModel.getSurfaceArea()
+        assertEquals(dummySurfaceArea, volume, 0.0001)
+    }
 
     @Test
     fun getCircumFerence() {
